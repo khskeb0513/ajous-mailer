2493b29ce9a9d99c8546bcc9a4b2cabfaa497ad4
HYEONSEUNG KANG <h5k@ajou.ac.kr>
2022-03-17 19:27:28 +0900

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { MailerController } from './mailer/mailer.controller';
+import { MailerService } from './mailer/mailer.service';
+
+@Module({
+  imports: [],
+  controllers: [MailerController],
+  providers: [MailerService],
+})
+export class AppModule {}
